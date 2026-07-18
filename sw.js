264dc4d98ad11e42eb9c89b6f7ba3763c8e75f32
Ваше имя <@.>
2026-07-18 07:51:28 +0600

    галочкановая

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kaspi-qr-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+const CACHE = 'kaspi-qr-v2';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './assets/галочка.svg', './assets/calf-promo.jpg', './assets/calf.jpg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
